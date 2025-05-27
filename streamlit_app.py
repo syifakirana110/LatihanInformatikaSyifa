2e5452cae7160951e6aa47a9ae632bb139de7d2c
syifakirana110 <26syifa0109@gmail.com>
2025-05-27 09:32:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,13 @@ st.write(
     "ngoding seru bersama Syifa Kirana"
 )
 st.image ("99f90fc29f3e7b35c70820491a5bbc35.jpg")
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
+    
